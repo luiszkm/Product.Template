84b881f2787cf933111958138d873c2fa40b4355
luis <luis.pereira@stoque.com.br>
2025-10-10 18:45:59 -0300

    Adiciona classe ApiResponseList para respostas paginadas
    Foi adicionada a classe `ApiResponseList<TItemData>` no arquivo
    `ApiResponseList.cs`, dentro do namespace
    `Product.Template.Api.ApiModels.Response`.
    
    A classe herda de `ApiResponse<IReadOnlyList<TItemData>>` e foi
    projetada para lidar com respostas que contêm listas de itens.
    
    Dois construtores foram implementados:
    - Um que aceita os parâmetros `currentPage`, `perPage`, `total` e
      `data` para inicializar a propriedade `Meta`.
    - Outro que aceita um objeto `PaginatedListOutput<TItemData>` e
      utiliza suas propriedades para inicializar `Meta`.
    
    A propriedade `Meta` foi adicionada como somente leitura e é do
    tipo `ApiResponseListMeta`. A classe também utiliza o namespace
    `Product.Template.Kernel.Application.Data`.

---
+++ src/Api/Api/ApiModels/Response/ApiResponseList.cs
@@ -0,0 +1,30 @@
+namespace Product.Template.Api.ApiModels.Response
+{
+    using Product.Template.Kernel.Application.Data;
+
+    public class ApiResponseList<TItemData> : ApiResponse<IReadOnlyList<TItemData>>
+    {
+        public ApiResponseList(
+            int currentPage,
+            int perPage,
+            int total,
+            IReadOnlyList<TItemData> data) : base(data)
+        {
+            Meta = new(
+                currentPage,
+                perPage,
+                total);
+        }
+        public ApiResponseList(
+            PaginatedListOutput<TItemData> paginatedListOutput
+            ) : base(paginatedListOutput.Items)
+        {
+            Meta = new ApiResponseListMeta(
+                paginatedListOutput.Page,
+                paginatedListOutput.PerPage,
+                paginatedListOutput.TotalCount);
+        }
+        public ApiResponseListMeta Meta { get; private set; }
+    }
+}
+
